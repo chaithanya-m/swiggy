45cc398ff1873d04a194a03240a6c8a6a87f4e8f
M Chaithanya <chaithanyamadduru03@gmail.com>
2023-12-12 18:10:25 +0530

    Revert "Revert "added scopes to Restaurant model""
    This reverts commit 50fbd184587eb3671ae8285455415595b7dc2960.

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -7,7 +7,7 @@ class AddressesController < ApplicationController
     @address=current_user.addresses.new
   end
   def create
-    @user = current_user  # Assuming you have a `current_user` method available
+    @user = current_user  
     @address = @user.addresses.new(address_params)
 
     if @address.save
@@ -18,7 +18,7 @@ class AddressesController < ApplicationController
   end
   
   def edit 
-    @user = current_user # Assuming you have a `current_user` method
+    @user = current_user 
     @address = @user.addresses.find(params[:id])
   end
   def update
@@ -31,8 +31,6 @@ class AddressesController < ApplicationController
       render :edit
     end
   end
-
-
 
   def destroy
     @user = current_user # Assuming you have a `current_user` method
