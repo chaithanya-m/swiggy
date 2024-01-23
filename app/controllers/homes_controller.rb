47667628350bac76893b7ba0cef66a5a778e4a64
M Chaithanya <chaithanyamadduru03@gmail.com>
2024-01-23 19:33:46 +0530

    after hard revert

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,42 +1,33 @@
 class HomesController < ApplicationController
     def index
-        # binding.pry
-
         @categories = Category.all
         session[:location] = Address.where(addressable_type: 'Restaurant').pluck(:street_area).uniq.first
         @restaurants = Restaurant.by_street_area(session[:location])
     end
+    
 
     def select_location
         # binding.pry
         session[:location] = params[:selected_location]
         @restaurants = Restaurant.by_street_area(session[:location])
         render 'index'
-
     end
+
     def filter
-        # binding.pry
         case params[:options_filter]
         when '300-600'
           @restaurants = Restaurant.by_street_area(session[:location]).price_range(300, 600)
           render 'index'
-
         when 'Below 300'
-
           @restaurants = Restaurant.by_street_area(session[:location]).below_price(300)
-
           render 'index'
         when 'New'
           @restaurants = Restaurant.by_street_area(session[:location]).newest_first
           render 'index'
-
         when 'Pure Veg'
           @restaurants = Restaurant.by_street_area(session[:location]).where("spaciality LIKE ? ", "%pure veg%" )
-
           render 'index'
-
         end
-
-
     end
+
 end
