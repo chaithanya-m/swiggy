b8ef30d1e578c46074f559a1e54c357b66ee0389
M Chaithanya <chaithanyamadduru03@gmail.com>
2024-01-23 19:33:46 +0530

    after hard revert

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,5 @@
 class HomesController < ApplicationController
     def index
-        # binding.pry
-
         @categories = Category.all
         @locations   = Address.locations
         if session[:location].nil?
@@ -9,38 +7,36 @@ class HomesController < ApplicationController
         end
         @restaurants = Restaurant.by_street_area(session[:location])
     end
+    
 
     def select_location
         # binding.pry
         session[:location] = params[:location]
         @restaurants = Restaurant.by_street_area(session[:location])
+<<<<<<< HEAD
         # redirect_to action: 'index'
         # render json: { success: true }.to_json
         redirect_to root_path
+=======
+        render 'index'
+>>>>>>> 4766762 (after hard revert)
     end
+
     def filter
-        # binding.pry
         case params[:options_filter]
         when '300-600'
           @restaurants = Restaurant.by_street_area(session[:location]).price_range(300, 600)
           render 'index'
-
         when 'Below 300'
-
           @restaurants = Restaurant.by_street_area(session[:location]).below_price(300)
-
           render 'index'
         when 'New'
           @restaurants = Restaurant.by_street_area(session[:location]).newest_first
           render 'index'
-
         when 'Pure Veg'
           @restaurants = Restaurant.by_street_area(session[:location]).where("spaciality LIKE ? ", "%pure veg%" )
-
           render 'index'
-
         end
-
-
     end
+
 end
